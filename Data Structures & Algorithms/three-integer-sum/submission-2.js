3302630a5c1ba35ecd2f448a37e1b680687365b9
Aster <117412836+Aster-Shan@users.noreply.github.com>
2026-05-22 19:20:57 +0100

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,56 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     * input are array of number
+     * output are triplets not duplicate
+     * brutefoce solution using loops
+     * O(n^3)
+     * two pointers =>o(n)
+     *
+     * sort the arary
+     * loop and remove duplicate
+     * define left and right for two pointers
+     * check if sum = nums[i]+left +right
+     * remove duplicates again for not getting same arrays output
+     */
+    threeSum(nums) {
+       const result=[];
+        nums.sort((a,b)=>a-b);
+        for(let i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]=== nums[i-1])continue;
+            let left =i+1;
+            let right=nums.length-1;
+
+            while(left<right){
+                const sum =nums[i]+nums[left]+nums[right];
+
+                if(sum===0){
+                    result.push([
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    ])
+                    while(left<right && nums[left]===nums[left+1]){
+                        left++;
+                    }
+                    while (left<right && nums[right]=== nums[right-1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+                else if(sum<0) {
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+
+
+        }
+        return result;
+
+    }
+}
